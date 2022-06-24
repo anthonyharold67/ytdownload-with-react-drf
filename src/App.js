865c7be3f51e4750a2cc39f923c17c4c5057e1b5
AnthonyHarold67 <anthonyharold67@gmail.com>
2022-06-24 16:06:37 +0300

    project added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import YoutubeVideo from './YoutubeVideo';
 
 function App() {
+  const [url,setUrl]=useState("")
+  const [video,setVideo]=useState(false)
+  const [loading,setLoading] = useState()
+
+  const getVideo=()=>{
+    setLoading(false)
+    console.log(url)
+    if(url){
+      
+      fetch(`http://127.0.0.1:8000/api/youtube?url=${url}`)
+      .then(res => res.json())
+      .then(data => {
+        setVideo(data)
+        setLoading(true)
+      })
+    }
+    
+    console.log(video)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="" method="get" onSubmit={e=> e.preventDefault()}>
+        <h3>Youtube Downloader</h3>
+        <div className="search">
+            <input type="text" id="url" onChange={e=>setUrl(e.target.value)} placeholder="Youtube URL"/>
+            <button type="submit" onClick={getVideo}>Download</button>
+        </div>
+    </form>
+    {loading===false && <div className="loading">Loading...</div>}
+    {video && <YoutubeVideo video={video}/>}
     </div>
   );
 }
---
+++ src/YoutubeVideo.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const YoutubeVideo = ({video}) => {
+    const {info,sources}=video
+  return (
+    <div className="result">
+        
+        <div className="video-item">
+            <img src={info.thumbnail} alt={info.title}/>
+            <div className="video-details">
+                <div className="title">{info.title}</div>
+                <div className="author"><span>Author:</span>{info.author} </div>
+                <div className="length"><span>Time:</span> {info.length}</div>
+                <div className="view"><span>Views</span> {info.views}</div>
+            </div>
+        </div>
+        <ul>
+            {sources.map((source,index)=>(
+                <li key={index}>
+                <div className="quality">
+                    <span>Quality</span>
+                    {source.quality}
+                </div>
+                <div className="size">
+                    <span>Size</span>
+                    {source.size}
+                </div>
+                <a href={source.url+"&title=" + info.title} target="_blank" rel='noreferrer' download={info.title}>Download</a>
+            </li>
+                ))
+            }
+            
+        </ul>
+
+    </div>
+  )
+}
+
+export default YoutubeVideo
